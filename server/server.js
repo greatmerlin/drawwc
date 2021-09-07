f397bd05fd2ab0818b4752dceed9d2d862589cd9
Theo <theologos.baxevanos@students.ffhs.ch>
2021-09-08 01:02:43 +0200

    client side

--- server/server.js
+++ server/server.js
@@ -2,8 +2,25 @@ var port = 8000;
        // Server code
        var WebSocketServer = require('ws').Server;
        var server = new WebSocketServer({ port: port });
+       
+       
+/*        server.on('connection', function(socket) {
+         console.log("A connection established");       
+}); */
+       var User = require('./game').User;
+       var Room = require('./game').Room;
+       var room1 = new Room();
        server.on('connection', function(socket) {
-         console.log("A connection established");
-});
+       var user = new User(socket);
+       room1.addUser(user);
+       console.log("A connection established");
+       var message = "Welcome " + user.id
+       + " joining the party. Total connection: "
+       + room1.users.length;
+       room1.sendAll(message);
+       });
+
+
        console.log("WebSocket server is running.");
        console.log("Listening to port " + port + ".");
+
